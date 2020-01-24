be06e73637e86db604715c2384444ebd3f83736b
OlegBalabukh <olegukoff@gmail.com>
2020-01-24 20:36:06 +0200

    GameField: changed id to class for square color

--- src/components/GameField/GameField.js
+++ src/components/GameField/GameField.js
@@ -1,42 +1,37 @@
-import React, {  useEffect } from 'react';
+import React, { useEffect } from 'react';
 import './GameField.css';
 
 const GameField = ({ gameStatus, setGreen, settingRed, cancelSettingRed }) => {
-
-  const { activeSquare, emptySquares, playerName, fieldSize } = gameStatus
-  const { gameField, delay, score } = gameStatus
+  const { activeSquare, emptySquares, playerName, fieldSize } = gameStatus;
+  const { gameField, delay, score } = gameStatus;
 
   useEffect(() => {
-    activeSquare.color === 'blue' && settingRed()
-  }, [activeSquare.color, gameField, settingRed])
+    activeSquare.color === 'blue' && settingRed();
+  }, [activeSquare.color, gameField, settingRed]);
 
   const onClickHandler = (id, color) => {
     if (color === 'blue') {
-      cancelSettingRed()
-      setGreen({ id, gameField, delay, emptySquares, score, playerName })
+      cancelSettingRed();
+      setGreen({ id, gameField, delay, emptySquares, score, playerName });
     }
-  }
+  };
 
   return (
-    <div id="field">
-      {
-        gameField &&
-        gameField.map((row, i) =>
-        <div className="row" key={i}>
-          {
-            row.map(square =>
-            <div
-              className={`square squareSize-${fieldSize}`}
-              key={square.id}
-              id={square.color}
-              onClick={() => onClickHandler(square.id, square.color)}
-            >
-            </div>)
-          }
-        </div>)
-      }
+    <div id='field'>
+      {gameField &&
+        gameField.map((row, i) => (
+          <div className='row' key={i}>
+            {row.map(square => (
+              <div
+                className={`square squareSize-${fieldSize} squareColor-${square.color}`}
+                key={square.id}
+                onClick={() => onClickHandler(square.id, square.color)}
+              ></div>
+            ))}
+          </div>
+        ))}
     </div>
-  )
-}
+  );
+};
 
 export default GameField;
